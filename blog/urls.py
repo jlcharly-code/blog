6595645f9e5c39ba9086bafc28af477202148f25
jlcharly-code <jlcharly@gmail.com>
2026-08-25 19:18:39 -0400

    ajout de detailview

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogHome
+from .views import BlogHome, PostDetailView  # <--- Ajoute PostDetailView ici
 
 app_name = "blog"
 
 urlpatterns = [
     path('', BlogHome.as_view(), name="home"),
+    # Ajoute cette ligne pour gérer les détails des articles par slug
+    path('post/<slug:slug>/', PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 class BlogHome(ListView):
@@ -10,3 +10,10 @@ class BlogHome(ListView):
         if self.request.user.is_authenticated:
             return queryset
         return queryset.filter(published=True)
+
+class PostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'  # Le nom du fichier HTML pour l'article complet
+    context_object_name = 'posts'
+    # Django utilise automatiquement le slug ou l'ID si tu ne specifies pas de field_name
+    # Comme ton URL utilise <slug:slug>, Django le détectera automatiquement.
